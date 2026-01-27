20ac0ca6aab4716274215eb7cb86021666d7df2b
Harry <harrydah29@gmail.com>
2026-01-27 17:14:41 +0100

    renamed main to Main to fit standard.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 
-abstract class main{
+abstract class Main{
+
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
